8df084ab6078aa167dedb4c440dab1ebe9ad4759
Felix Prima <primafelix1207@gmail.com>
2022-03-31 16:48:37 +0700

    inbox

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
                         <a class="nav-link" href = "<?= BASE_URL . "group/mygroup.php" ?>" >Groups</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#"> Assignments</a>
+                        <a class="nav-link" href="#"> Inbox</a>
                     </li>
 
                     <li class="nav-item">

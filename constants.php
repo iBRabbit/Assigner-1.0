fff7fb8d208e482aff607b228bb541968a0b98cb
Felix Prima <felix.prima@binus.ac.id>
2023-05-22 15:05:06 +0700

    Removed unecessary items.

--- constants.php
+++ constants.php
@@ -3,18 +3,11 @@
 define("DB_HOSTNAME", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "seproject");
-
-// define("DB_NAME", 'epiz_31392697_seproject');
-// define("DB_PASSWORD", 'Wv1ffgDDrSzDK9W');
-// define("DB_HOSTNAME", 'sql213.epizy.com');
-// define("DB_USERNAME", 'epiz_31392697');
+define("DB_NAME", "assigner");
 
 define("GROUPINVITE_NOTIF", 1);
 define("ASSIGNMENT_NOTIF", 2);
 
-define("BASE_URL", "http://localhost/SEProject/");
-// define("BASE_URL", "http://assigner.rf.gd/");
-
+define("BASE_URL", "http://localhost/assigner/");
 ?>
 
